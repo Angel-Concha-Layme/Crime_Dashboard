5b8fd6489674fbdf9c236878c2028d921827ccfa
Angel Concha Layme <aconchal@unsa.edu.pe>
2022-07-07 12:09:32 -0500

    Crime Dashboard

---
+++ js/filters.js
@@ -0,0 +1,83 @@
+$(document).ready(
+
+	function() {
+	var offSelect = $("#crimeType")
+	
+	$.ajax({
+		url: 'https://data.cityofchicago.org/resource/3i3m-jwuy.geojson?$select=distinct primary_type&$order=primary_type ASC'
+	})
+	.done(function(data) {
+		$.each(data.features, function (i, feature) {
+
+			offSelect.append(
+				$("<option></option>").val(feature.properties.primary_type).html(feature.properties.primary_type))
+		});
+
+
+	})
+	.fail(function() {
+	})
+	.always(function() {
+	});
+
+
+	var arrSelect = $("#arrest")
+	$.ajax({
+		url: 'https://data.cityofchicago.org/resource/3i3m-jwuy.geojson?$select=distinct arrest&$order=arrest DESC'
+	})
+	.done(function(data) {
+		$.each(data.features, function (i, feature) {
+			arrSelect.append(
+				$("<option></option>").val(feature.properties.arrest).html(feature.properties.arrest.toString().toUpperCase()))
+		});
+	})
+	.fail(function() {
+	})
+	.always(function() {
+	});
+
+	var domSelect = $("#domestic")
+	$.ajax({
+		url: 'https://data.cityofchicago.org/resource/3i3m-jwuy.geojson?$select=distinct domestic&$order=domestic DESC'
+	})
+	.done(function(data) {
+		$.each(data.features, function (i, feature) {
+			domSelect.append(
+				$("<option></option>").val(feature.properties.domestic).html(feature.properties.domestic.toString().toUpperCase()))
+		});
+	})
+	.fail(function() {
+	})
+	.always(function() {
+
+	});
+
+
+    $('#startDate').datepicker({
+    	showAnim: 'slideDown',
+    	changeMonth: true,
+    	numberOfMonths: 1,
+    	dateFormat: 'yy-mm-dd',
+    	changeYear: true,
+    	defaultDate: new Date(2010, 01, 01),
+    	onClose: function( selectedDate ) {
+    		$( "#endDate" ).datepicker( "option", "minDate", selectedDate );
+		}
+	});
+
+    $('#endDate').datepicker({
+    	defaultDate: "+1w",
+		changeMonth: true,
+		numberOfMonths: 1,
+		dateFormat: 'yy-mm-dd',
+		changeYear: true,
+		defaultDate: new Date(),
+		onClose: function( selectedDate ) {
+    		$( "#startDate" ).datepicker( "option", "maxDate", selectedDate );
+	  	},
+	});
+
+
+});
+
+var app_token = "GhcK7Cj79Lg5uRtd5hPdldrnY";
---
+++ js/tableData.js
@@ -0,0 +1,60 @@
+function main() {
+var app_token = "GhcK7Cj79Lg5uRtd5hPdldrnY";
+
+	$('.form-control').change(function() {
+		var offence= $("#crimeType").val();
+		var arrest= $("#arrest").val();
+		var domestic= $("#domestic").val();
+		var startDate= $("#startDate").val();
+		var endDate= $("#endDate").val();
+
+	// ---------- Set request parameters ---------------
+		var formTable = new FormData();
+
+	    var formTable =  {
+	        	"$where" : "date >='" + startDate + "'"
+	        	+ " AND date <='" + endDate + "'"
+	        	+ " AND latitude IS NOT NULL",
+	        	"arrest" : arrest,
+	        	"domestic" : domestic,
+				"primary_type" : offence,
+				"$limit": 2000,
+	        	"$$app_token" : app_token};
+
+	        if (offence == "All") {
+			  delete formTable.primary_type;
+			}if (arrest == "All"){
+				delete formTable.arrest;
+			}if (domestic == "All"){
+				delete formTable.domestic;
+			}else{
+
+			};
+
+		$.ajax({
+			url: 'https://data.cityofchicago.org/resource/3i3m-jwuy.geojson',
+			method: "GET",
+			dataType: "json",
+	        data: formTable,
+		}).done(function(data) {
+			var table = data.features
+			$('#myTable').DataTable({
+				destroy: true, //clear previous table//
+				data: table,
+			    columns: [
+		            { "data": "properties.primary_type" },
+		            { "data": "properties.description" },
+		            { "data": "properties.location_description" },
+		            { "data": "properties.date" },
+		            { "data": "properties.arrest" },
+		            { "data": "properties.domestic" },
+		            { "data": "properties.community_area" },
+		            { "data": "properties.latitude" },
+		            { "data": "properties.longitude" },
+		        ]
+			});
+		});
+	}).change();
+};
+
+window.onload = main;
