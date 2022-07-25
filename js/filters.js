5b88b2df3d035ebc42f34a93cfe92485f4efa96e
Angel Concha Layme <aconchal@unsa.edu.pe>
2022-07-25 13:33:22 -0500

    Update filters.js

--- js/filters.js
+++ js/filters.js
@@ -59,7 +59,7 @@ $(document).ready(
     	numberOfMonths: 1,
     	dateFormat: 'yy-mm-dd',
     	changeYear: true,
-    	defaultDate: new Date(2010, 01, 01),
+    	defaultDate: new Date(2002, 01, 01),
     	onClose: function( selectedDate ) {
     		$( "#endDate" ).datepicker( "option", "minDate", selectedDate );
 		}
